278893216fff66f10d80e796e75cbcc266012e81
zubairk22oct <areeb9670@gmail.com>
2022-02-23 11:28:15 +0530

    Add Props for Elixir official

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,4 +35,4 @@ ODM_MANIFEST_GALAHAD_FILES := $(DEVICE_PATH)/manifest_galahad.xml
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
